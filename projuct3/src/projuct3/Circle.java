1617eb1c5776dc02b1d7c35c44905910fbd0fc5e
김보희 <@DESKTOP-MU168EP>
2021-06-21 10:31:19 +0900

    Modify of Circle code

--- projuct3/src/projuct3/Circle.java
+++ projuct3/src/projuct3/Circle.java
@@ -7,24 +7,13 @@ public class Circle implements GeometricObject {
 		this.radius=radius;
 	}
 	
-	public double getPerimeter() {
-		return (radius*2*Math.PI);
+	public double get_Perimeter() {
+		return radius*2*Math.PI;
 	}
 	
-	public double getArea() {
-		return (radius*radius*Math.PI);
-	}
-
-	@Override
-	public double get_Perimeter() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public double get_Area() {
-		// TODO Auto-generated method stub
-		return 0;
+		return radius*radius*Math.PI;
 	}
+
 
 }
