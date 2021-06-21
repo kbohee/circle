c4cc7d4e5752ab39532bc1a2a1fe3261726218b2
김보희 <@DESKTOP-MU168EP>
2021-06-21 10:13:38 +0900

    It's Official code for perimeter and width

---
+++ projuct3/src/projuct3/Circle.java
@@ -0,0 +1,30 @@
+package projuct3;
+
+public class Circle implements GeometricObject {
+	protected double radius=1.0;
+	
+	public Circle(double radius) {
+		this.radius=radius;
+	}
+	
+	public double getPerimeter() {
+		return radius*2*Math.PI;
+	}
+	
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+
+	@Override
+	public double get_Perimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double get_Area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
